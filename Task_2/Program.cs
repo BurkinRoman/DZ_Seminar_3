df9b147dea3b2a8a9544b5c9d9183a7c1c27daa8
Roman Burkin <roman9969@gmail.com>
2023-12-01 22:11:12 +0300

    Completed Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Задаем массив из 10 целых чисел
+        int[] arr = { 5, 12, 7, 14, 23, 30, 18, 42, 56, 91 };
+
+        // Находим количество четных чисел в массиве
+        int countEven = 0;
+
+        foreach (int num in arr)
+        {
+            if (num % 2 == 0)
+            {
+                countEven++;
+            }
+        }
+
+        Console.WriteLine($"Количество четных чисел в массиве: {countEven}");
+    }
+}
